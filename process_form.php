5f860e657ee362503a2b8ef1be4dbb4571c05a15
Shree Krishna Devkota <shreekrishna1514@gmail.com>
2024-05-31 12:11:18 +0545

    Add files via upload

---
+++ process_form.php
@@ -0,0 +1,23 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve form data
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    // Send email (example)
+    $to = "shreekrishna1514@gmail.com";
+    $subject = "New Message from Website";
+    $body = "Name: $name\nEmail: $email\nMessage:\n$message";
+    $headers = "From: $email";
+
+    // Uncomment the line below to send the email
+     mail($to, $subject, $body, $headers);
+
+    // Store in a database (example)
+    // You need to set up your database connection here
+    // $conn = new mysqli($servername, $username, $password, $dbname);
+    // $sql = "INSERT INTO messages (name, email, message) VALUES ('$name', '$email', '$message')";
+    // $conn->query($sql);
+}
+?>
